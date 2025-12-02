e8604d01426933957d7916d800f31f51b8daf4b4
EZZ-0 <eyadzainy@gmail.com>
2025-12-02 08:52:18 -0500

    Create logging_config.py

---
+++ utils/logging_config.py
@@ -0,0 +1,11 @@
+"""
+Utils package for Saudi Earnings Engine
+"""
+
+from .security import SecurityValidator, quick_validate, sanitize
+from .logging_config import EngineLogger, get_logger, log_error, log_warning, log_info
+
+__all__ = [
+    'SecurityValidator', 'quick_validate', 'sanitize',
+    'EngineLogger', 'get_logger', 'log_error', 'log_warning', 'log_info'
+]
